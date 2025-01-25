8e61e911946f5eebe935c8be7cb0f1bc04c37b4d
pranathi vankala <pranathivankala010@gmail.com>
2025-01-25 16:17:24 +0530

    added files

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/Talent96", {
+    await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
