eb350ab076e47cbcc6096e28c0c9608af090a4c3
pranathi vankala <pranathivankala010@gmail.com>
2025-02-08 17:19:08 +0530

    add changes to db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,11 @@
-const mongoose = require("mongoose");
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true, // Ensures the URL parser used is modern
+      useUnifiedTopology: true, // Ensures the connection to the server is handled properly
     });
     console.log("Database connected successfully");
   } catch (error) {
