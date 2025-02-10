2034709a6a8a4b5553908dd0030a5573d478812e
pranathi vankala <pranathivankala010@gmail.com>
2025-02-10 14:47:36 +0530

    added changes in db.js

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,11 @@ const userRoutes = require("./Routes/userRoutes");
 const app = express();
 
 app.use(express.json()); 
-app.use(cors()); 
+app.use(cors({
+  origin: ["https://talent96.com","http://localhost:3000"],
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true
+})); 
 
 connectDB();
 
